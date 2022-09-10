52684ef9d053a87dbcbb94e30792b1a8847d78c1
PremKumbhar <kumbharprem7738@gmail.com>
2022-09-10 14:55:15 +0530

    [Prem]:Added handle exception user given null mood should return HAPPY

--- src/main/java/com/bridgelabz/MoodAnalyser/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyser/MoodAnalyzer.java
@@ -2,16 +2,23 @@ package com.bridgelabz.MoodAnalyser;
 
 public class MoodAnalyzer {
     private String message;
+    public MoodAnalyzer() {
+    }
 
-    public MoodAnalyzer(String message){
+    public MoodAnalyzer(String message) {
         this.message = message;
         analyseMood();
     }
 
     public String analyseMood() {
-        if (message.contains("sad"))
-            return "SAD";
-        else
-            return "HAPPY";
+        try {
+            if (message.contains("sad"))
+                return "SAD";
+            else if (message.contains("happy"))
+                return "HAPPY";
+
+        } catch (NullPointerException exception) {
+        }
+        return "HAPPY";
     }
 }
--- src/test/java/com/bridgelabz/MoodAnalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyser/MoodAnalyserTest.java
@@ -6,7 +6,23 @@ import org.junit.jupiter.api.Test;
 public class MoodAnalyserTest {
     @Test
     public void testMoodAnalysis() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in sad mood");
+        String actualResult = moodAnalyzer.analyseMood();
+        Assertions.assertEquals("SAD", actualResult);
+    }
+
+    /**
+     * This method will return Happy if we pass as in any mood.
+     */
+    @Test
+    public void givenHappyMessage_shouldReturnHappy() {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in happy mood");
+        String actualResult = moodAnalyzer.analyseMood();
+        Assertions.assertEquals("HAPPY", actualResult);
+    }
+    @Test
+    public void givenNullMessage_shouldReturnInvalid(){
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(" ");
         String actualResult = moodAnalyzer.analyseMood();
         Assertions.assertEquals("HAPPY", actualResult);
     }
